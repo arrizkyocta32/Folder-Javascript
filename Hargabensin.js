5fc9918a2e9ed0517eba72b4baf781593d4ced31
arrizkyocta32 <144558025+arrizkyocta32@users.noreply.github.com>
2024-02-01 23:06:23 +0700

    Add files via upload

---
+++ Hargabensin.js
@@ -0,0 +1,16 @@
+const hargaBensinNaik ={
+    0: 'Pertalite 10000',
+    1: 'Pertamax 12500',
+    2: 'Solar 8500',
+};
+const hargaBensinTurun = [
+    'Pertalite 7500',
+    'Pertamax 9500',
+    'Solar 5500',
+];
+console.log(hargaBensinNaik[0]);
+console.log(hargaBensinTurun[0]);
+console.log(hargaBensinNaik[1]);
+console.log(hargaBensinTurun[1]);
+console.log(hargaBensinNaik[2]);
+console.log(hargaBensinTurun[2]);
---
+++ kehadiran.js
@@ -0,0 +1,19 @@
+const kehadiran = [
+    {
+        name : 'Andi',
+        class : 12,
+        isAttendance : true,
+    },
+    {
+        name : 'Della',
+        class :12,
+        isAttendance :true,
+    },
+    {
+        name : 'Joko',
+        class :12,
+        isAttendance : false,
+    },
+];
+console.log(kehadiran[1].class);
+console.log(kehadiran[2].isAttendance)
